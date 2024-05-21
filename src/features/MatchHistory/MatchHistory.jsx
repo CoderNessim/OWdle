90c0fe0959d15df3a57cb505411708102aa1278a
Nessim Yohros <nessim@MacBook-Pro-109.local>
2024-05-21 16:51:33 -0400

    finished pagination for match history

--- src/features/MatchHistory/MatchHistory.jsx
+++ src/features/MatchHistory/MatchHistory.jsx
@@ -3,13 +3,30 @@ import { useGameHistory } from '../../hooks/useGameHistory';
 import NoMatchHistory from './NoMatchHistory';
 import Match from './Match';
 import styles from './MatchHistory.module.css';
+import { useSearchParams } from 'react-router-dom';
 
 function MatchHistory() {
   const { gameHistory, isPending } = useGameHistory();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const itemsPerPage = 3;
+
   if (isPending) return <Loader />;
   if (!gameHistory?.games?.length) return <NoMatchHistory />;
 
+  const currentPage = parseInt(searchParams.get('page')) || 1;
+
   const parsedGameHistory = JSON.parse(gameHistory.games);
+
+  const totalGames = parsedGameHistory.length;
+  const totalPages = Math.ceil(totalGames / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentGames = parsedGameHistory.slice(startIndex, endIndex);
+
+  function handlePageChange(page) {
+    setSearchParams({ page });
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -18,12 +35,16 @@ function MatchHistory() {
           Match History
         </Title>
         <Stack className={styles.matchStack}>
-          {parsedGameHistory.map((game, index) => (
+          {currentGames.map((game, index) => (
             <Match key={index} game={game} />
           ))}
         </Stack>
       </div>
-      <Pagination />
+      <Pagination
+        total={totalPages}
+        page={currentPage}
+        onChange={handlePageChange}
+      />
     </div>
   );
 }
